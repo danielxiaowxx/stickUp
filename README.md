a87971e4c21521798c3f90e88c4b436c441ce37c
daniel <xiaohutu136@163.com>
2013-11-28 09:51:28 +0800

    update

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ a jQuery plugin
 A simple plugin that "sticks" an element to the top of the browser window while scrolling past it, always keeping it in view. This plugin works on multi-page sites, but has additional features for one-pager layouts.
 
 Please Visit the <a href="http://lirancohen.github.io/stickUp">GitHub Page</a> for installation instructions.
+
+## Daniel Improvement
+1. 元素居顶固定时的宽度保持与原来不变
+2. 增加fixTopVal option：元素居顶固定时与顶部的距离
+3. 精简了滚动时变化的代码
+4. 支持同一个页面多个元素进行居顶固定
--- stickUp.js
+++ stickUp.js
@@ -1,122 +1,122 @@
-jQuery(
-function($) {
-	
-	$(document).ready(function(){
-		var contentButton = [];
-		var contentTop = [];
-		var content = [];
-		var lastScrollTop = 0;
-		var scrollDir = '';
-		var itemClass = '';
-		var itemHover = '';
-		var menuSize = null;
-		var stickyHeight = 0;
-		var stickyMarginB = 0;
-		var currentMarginT = 0;
-		var topMargin = 0;
-		$(window).scroll(function(event){
-   			var st = $(this).scrollTop();
-   			if (st > lastScrollTop){
-       			scrollDir = 'down';
-   			} else {
-      			scrollDir = 'up';
-   			}
-  			lastScrollTop = st;
-		});
-		$.fn.stickUp = function( options ) {
-			// adding a class to users div
-			$(this).addClass('stuckMenu');
-        	//getting options
-        	var objn = 0;
-        	if(options != null) {
-	        	for(var o in options.parts) {
-	        		if (options.parts.hasOwnProperty(o)){
-	        			content[objn] = options.parts[objn];
-	        			objn++;
-	        		}
-	        	}
-	  			if(objn == 0) {
-	  				console.log('error:needs arguments');
-	  			}
+(function() {
 
-	  			itemClass = options.itemClass;
-	  			itemHover = options.itemHover;
-	  			if(options.topMargin != null) {
-	  				if(options.topMargin == 'auto') {
-	  					topMargin = parseInt($('.stuckMenu').css('margin-top'));
-	  				} else {
-	  					if(isNaN(options.topMargin) && options.topMargin.search("px") > 0){
-	  						topMargin = parseInt(options.topMargin.replace("px",""));
-	  					} else if(!isNaN(parseInt(options.topMargin))) {
-	  						topMargin = parseInt(options.topMargin);
-	  					} else {
-	  						console.log("incorrect argument, ignored.");
-	  						topMargin = 0;
-	  					}	
-	  				}
-	  			} else {
-	  				topMargin = 0;
-	  			}
-	  			menuSize = $('.'+itemClass).size();
-  			}			
-			stickyHeight = parseInt($(this).height());
-			stickyMarginB = parseInt($(this).css('margin-bottom'));
-			currentMarginT = parseInt($(this).next().closest('div').css('margin-top'));
-			vartop = parseInt($(this).offset().top);
-			//$(this).find('*').removeClass(itemHover);
-		}
-		$(document).on('scroll', function() {
-			varscroll = parseInt($(document).scrollTop());
-			if(menuSize != null){
-				for(var i=0;i < menuSize;i++)
-				{
-					contentTop[i] = $('#'+content[i]+'').offset().top;
-					function bottomView(i) {
-						contentView = $('#'+content[i]+'').height()*.4;
-						testView = contentTop[i] - contentView;
-						//console.log(varscroll);
-						if(varscroll > testView){
-							$('.'+itemClass).removeClass(itemHover);
-							$('.'+itemClass+':eq('+i+')').addClass(itemHover);
-						} else if(varscroll < 50){
-							$('.'+itemClass).removeClass(itemHover);
-							$('.'+itemClass+':eq(0)').addClass(itemHover);
-						}
-					}
-					if(scrollDir == 'down' && varscroll > contentTop[i]-50 && varscroll < contentTop[i]+50) {
-						$('.'+itemClass).removeClass(itemHover);
-						$('.'+itemClass+':eq('+i+')').addClass(itemHover);
-					}
-					if(scrollDir == 'up') {
-						bottomView(i);
-					}
-				}
-			}
+    /**
+     * 修改者：Daniel
+     * 修复地方：
+     * 1.元素居顶固定时的宽度保持不变
+     * 2.增加fixTopVal option：元素居顶固定时与顶部的距离
+     * 3.精简了滚动时变化的代码
+     * 4.支持同一个页面多个
+     *
+     */
+    $.fn.stickUp = function( options ) {
+
+        var contentTop = [];
+        var content = [];
+        var lastScrollTop = 0;
+        var scrollDir = '';
+        var itemClass = '';
+        var itemHover = '';
+        var menuSize = null;
+        var stickyWidth = 0;
+        var topMargin = 0;
+        var fixTopVal = 0;
+        var element = null;
+
+        $(window).scroll(function(){
+            var st = element.scrollTop();
+            if (st > lastScrollTop){
+                scrollDir = 'down';
+            } else {
+                scrollDir = 'up';
+            }
+            lastScrollTop = st;
+        });
+
+        // 初始化组件
+        element = $(this);
+        // adding a class to users div
+        element.addClass('stuckMenu');
+        //getting options
+        var objn = 0;
+        if(options != null) {
+            for(var o in options.parts) {
+                if (options.parts.hasOwnProperty(o)){
+                    content[objn] = options.parts[objn];
+                    objn++;
+                }
+            }
+
+            itemClass = options.itemClass;
+            itemHover = options.itemHover;
+            fixTopVal = options.fixTopVal ? options.fixTopVal.replace('px', '') : 0;
+            if(options.topMargin != null) {
+                if(options.topMargin == 'auto') {
+                    topMargin = parseInt(element.css('margin-top'));
+                } else {
+                    if(isNaN(options.topMargin) && options.topMargin.search("px") > 0){
+                        topMargin = parseInt(options.topMargin.replace("px",""));
+                    } else if(!isNaN(parseInt(options.topMargin))) {
+                        topMargin = parseInt(options.topMargin);
+                    } else {
+                        console.log("incorrect argument, ignored.");
+                        topMargin = 0;
+                    }
+                }
+            } else {
+                topMargin = 0;
+            }
+            menuSize = $('.'+itemClass).size();
+        }
+        stickyWidth = parseInt(element.width());
+        vartop = parseInt(element.offset().top);
 
 
+        $(document).on('scroll', function() {
+            varscroll = parseInt($(document).scrollTop());
+            if(menuSize != null){
+                for(var i=0;i < menuSize;i++)
+                {
+                    contentTop[i] = $('#'+content[i]+'').offset().top;
+                    function bottomView(i) {
+                        contentView = $('#'+content[i]+'').height()*.4;
+                        testView = contentTop[i] - contentView;
+                        //console.log(varscroll);
+                        if(varscroll > testView){
+                            $('.'+itemClass).removeClass(itemHover);
+                            $('.'+itemClass+':eq('+i+')').addClass(itemHover);
+                        } else if(varscroll < 50){
+                            $('.'+itemClass).removeClass(itemHover);
+                            $('.'+itemClass+':eq(0)').addClass(itemHover);
+                        }
+                    }
+                    if(scrollDir == 'down' && varscroll > contentTop[i]-50 && varscroll < contentTop[i]+50) {
+                        $('.'+itemClass).removeClass(itemHover);
+                        $('.'+itemClass+':eq('+i+')').addClass(itemHover);
+                    }
+                    if(scrollDir == 'up') {
+                        bottomView(i);
+                    }
+                }
+            }
 
-			if(vartop < varscroll + topMargin){
-				$('.stuckMenu').addClass('isStuck');
-				$('.stuckMenu').next().closest('div').css({
-					'margin-top': stickyHeight + stickyMarginB + currentMarginT + 'px'
-				}, 10);
-				$('.stuckMenu').css("position","fixed");
-				$('.isStuck').css({
-					top: '0px'
-				}, 10, function(){
+            // 向下滚动超过一定距离，元素开始固定
+            if(vartop < varscroll + topMargin){
 
-				});
-			};
+                if (stickyWidth != element.width()) {
+                    stickyWidth = element.width();
+                }
 
-			if(varscroll + topMargin < vartop){
-				$('.stuckMenu').removeClass('isStuck');
-				$('.stuckMenu').next().closest('div').css({
-					'margin-top': currentMarginT + 'px'
-				}, 10);
-				$('.stuckMenu').css("position","relative");
-			};
+                element.css("position","fixed").width(stickyWidth).css({
+                    top: fixTopVal + 'px'
+                }, 10);
+            };
 
-		});
-	});
+            // 向上滚动超过一定距离，元素开始恢复原来的位置
+            if(varscroll + topMargin < vartop){
+                element.css({"position":"relative", width:'', top:''});
+            };
+        });
+    }
+})($);
 
-});
